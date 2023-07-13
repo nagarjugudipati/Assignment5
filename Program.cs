5d48cf9e0a9765ae042523339b0394770a6c3d10
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-07-13 14:56:04 +0530

    Assignment5

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string correctUsername = "john";
+            string correctPassword = "pass123";
+            int attemptsRemaining = 3;
+
+            while (attemptsRemaining > 0)
+            {
+                Console.Write("Enter username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Enter password: ");
+                string password = Console.ReadLine();
+
+                if (username == correctUsername && password == correctPassword)
+                {
+                    Console.WriteLine("Login successful.");
+                    return;
+                }
+                else if (username == correctUsername)
+                {
+                    Console.WriteLine("Invalid password.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username.");
+                }
+
+                attemptsRemaining--;
+            }
+
+            Console.WriteLine("Login failed.");
+            Console.ReadKey();
+
+        }
+    }
+}
